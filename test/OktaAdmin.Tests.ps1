02e63c1a9b3b8508c339d873b8ba68118028879d
Nate Scherer <376408+natescherer@users.noreply.github.com>
2020-11-06 13:54:36 -0500

    Add basic test of manifest validity

--- test/OktaAdmin.Tests.ps1
+++ test/OktaAdmin.Tests.ps1
@@ -1,17 +1,14 @@
-$ModuleName = Split-Path -Path ($PSCommandPath -replace '\.Tests\.ps1$','') -Leaf
-$ModulePath = "$(Split-Path -Path $PSScriptRoot -Parent)\src\$ModuleName.psm1"
+$global:ModuleName = Split-Path -Path ($PSCommandPath -replace '\.Tests\.ps1$','') -Leaf
+$global:ModulePath = "$(Split-Path -Path $PSScriptRoot -Parent)\src\$ModuleName.psm1"
+$global:ModuleManifestPath = "$(Split-Path -Path $PSScriptRoot -Parent)\src\$ModuleName.psd1"
 Get-Module -Name $ModuleName -All | Remove-Module -Force -ErrorAction Ignore
 Import-Module -Name $ModulePath -Force -ErrorAction Stop
 
 InModuleScope $ModuleName {
-    $ModuleName = Split-Path -Path ($PSCommandPath -replace '\.Tests\.ps1$','') -Leaf
-    $ModulePath = "$(Split-Path -Path $PSScriptRoot -Parent)\src\$ModuleName.psm1"
-    $ModuleManifestPath = "$(Split-Path -Path $PSScriptRoot -Parent)\src\$ModuleName.psd1"
-
     Describe 'Module Manifest Tests' {
         It 'Passes Test-ModuleManifest' {
-            Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
-            $? | Should Be $true
+            Test-ModuleManifest -Path $ModuleManifestPath | Should -Not -BeNullOrEmpty
+            $? | Should -Be $true
         }
     }
 }
