7abe53fe1a9343c3abc325130925588f5079e2b1
Nate Scherer <376408+natescherer@users.noreply.github.com>
2020-11-06 13:54:20 -0500

    Move functionality into pester tests

--- loadmodule.ps1
+++
@@ -1,4 +0,0 @@
-$ModuleName = "OktaAPI"
-$ModulePath = "$PSScriptRoot\Modules\$ModuleName.psm1"
-Get-Module -Name $ModuleName -All | Remove-Module -Force -ErrorAction Ignore
-Import-Module -Name $ModulePath -Force -ErrorAction Stop
